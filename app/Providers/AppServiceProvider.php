e8aa0ad35ee4cfc6ff87403981d52aa77d05f9da
nooopallll <naufalzafrany@gmail.com>
2026-02-26 03:23:15 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (str_contains(request()->url(), 'sharedwithexpose.com')) {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
     }
